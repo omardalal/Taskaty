30a68a00abec3898b7c07e4f61dbb06c023272cf
Mohamed12Adra <ope3@hotmail.com>
2022-06-21 15:42:52 +0300

    files modified

--- src/Utilities/uploadFilesUtils.js
+++ src/Utilities/uploadFilesUtils.js
@@ -12,17 +12,18 @@ import { getFirebaseDb } from "./FirebaseUtils";
 // upload files for specific task
 export const uploadFileForTask = async (file, taskId) => {
   const taskRef = doc(getFirebaseDb(), "Task", taskId);
-
-  const fileRef = ref(getStorage(), "files/" + file.name + v4());
-  return await uploadBytes(fileRef, file).then(async () => {
-    await updateDoc(taskRef, {
-      files: arrayUnion({
-        date: Timestamp.now(),
-        fileName: file.name,
-        link: await getDownloadURL(fileRef)
-      })
+  for (var i = 0; i < file.length; i++) {
+    const fileRef = ref(getStorage(), "files/" + file[i].name + v4());
+    await uploadBytes(fileRef, file[i]).then(async () => {
+      await updateDoc(taskRef, {
+        files: arrayUnion({
+          date: Timestamp.now(),
+          fileName: file[i].name,
+          link: await getDownloadURL(fileRef)
+        })
+      });
     });
-  });
+  }
 };
 // upload files for specific task submission
 export const uploadFileForTaskSubmission = async (file, taskSubmissionId) => {
@@ -31,16 +32,18 @@ export const uploadFileForTaskSubmission = async (file, taskSubmissionId) => {
     "Task Submission",
     taskSubmissionId
   );
-  const fileRef = ref(getStorage(), "files/" + file.name + v4());
-  return await uploadBytes(fileRef, file).then(async () => {
-    await updateDoc(taskSubmissionRef, {
-      files: arrayUnion({
-        fileType: file.type,
-        fileName: file.name,
-        link: await getDownloadURL(fileRef)
-      })
+  for (var i = 0; i < file.length; i++) {
+    const fileRef = ref(getStorage(), "files/" + file[i].name + v4());
+    await uploadBytes(fileRef, file[i]).then(async () => {
+      await updateDoc(taskSubmissionRef, {
+        files: arrayUnion({
+          fileType: file[i].type,
+          fileName: file[i].name,
+          link: await getDownloadURL(fileRef)
+        })
+      });
     });
-  });
+  }
 };
 // upload files for specific project submission
 export const uploadFileForProjectSubmission = async (
@@ -52,42 +55,45 @@ export const uploadFileForProjectSubmission = async (
     "ProjectSubmission",
     projectSubmissionId
   );
-
-  const fileRef = ref(getStorage(), "files/" + file.name + v4());
-  return await uploadBytes(fileRef, file).then(async () => {
-    await updateDoc(projectSubmissionRef, {
-      files: arrayUnion({
-        date: Timestamp.now(),
-        fileType: file.type,
-        fileName: file.name,
-        link: await getDownloadURL(fileRef)
-      })
+  for (var i = 0; i < file.length; i++) {
+    const fileRef = ref(getStorage(), "files/" + file[i].name + v4());
+    await uploadBytes(fileRef, file).then(async () => {
+      await updateDoc(projectSubmissionRef, {
+        files: arrayUnion({
+          date: Timestamp.now(),
+          fileType: file[i].type,
+          fileName: file[i].name,
+          link: await getDownloadURL(fileRef)
+        })
+      });
     });
-  });
+  }
 };
-// upload files for specific announcement
+// upload files for specific class and announcement
 export const uploadFileForAnnouncement = async (file, classId, index) => {
   const classRef = doc(getFirebaseDb(), "Class", classId);
   const classDoc = await getDoc(classRef);
   const classDaTa = classDoc.data();
   const announcement = classDaTa?.announcements;
   console.log(announcement);
-  const fileRef = ref(getStorage(), "files/" + file.name + v4());
+  for (var i = 0; i < file.length; i++) {
+    const fileRef = ref(getStorage(), "files/" + file[i].name + v4());
 
-  try {
-    await uploadBytes(fileRef, file);
-    const announcementFiles = announcement[index]?.files;
-    const downloadURL = await getDownloadURL(fileRef);
-    announcementFiles?.push({
-      date: Timestamp.now(),
-      fileName: file.name,
-      fileType: file.type,
-      link: downloadURL
-    });
-    await updateDoc(classRef, {
-      announcements: { ...announcement, files: announcementFiles }
-    });
-  } catch (err) {
-    // yes3edha
+    try {
+      await uploadBytes(fileRef, file[i]);
+      const announcementFiles = announcement[index]?.files;
+      const downloadURL = await getDownloadURL(fileRef);
+      announcementFiles?.push({
+        date: Timestamp.now(),
+        fileName: file[i].name,
+        fileType: file[i].type,
+        link: downloadURL
+      });
+      await updateDoc(classRef, {
+        announcements: { ...announcement, files: announcementFiles }
+      });
+    } catch (err) {
+      // yes3edha
+    }
   }
 };
